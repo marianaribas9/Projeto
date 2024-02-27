31fecb753176e3939a887da7796e12a240dd69b3
Mariana <mariana.ribas.cardoso@escola.pr.gov.br>
2024-02-27 11:11:06 -0300

    projeto3.27/02

---
+++ projetoboolean.js
@@ -0,0 +1,19 @@
+const primeironumero = 5;
+const segundonumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
